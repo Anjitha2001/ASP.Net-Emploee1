eaf00fe816f6e7d1d7a565fd0c074b7e76f213a1
ANJITHA RAJ <anjitharaj29@gmail.com>
2025-07-01 08:42:33 +0530

    Add files via upload

--- EmployeeModuleController.cs
+++ EmployeeModuleController.cs
@@ -1,4 +1,5 @@
 ﻿using Business.Logic;
+using DataAccess.Models;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 
@@ -16,7 +17,7 @@ namespace ApiExample1.Controllers
             _employeeModuleLogic = employeeModuleLogic;
         }
         [HttpGet("getName/{name}")]
-        public string  getName([FromRoute] string name)
+        public string getName([FromRoute] string name)
         {
             return _employeeModuleLogic.GetNameLogic(name);
         }
@@ -32,7 +33,7 @@ namespace ApiExample1.Controllers
             return Ok(EmployeeDetails);
         }
         [HttpGet("GetDetails2")]
-        public async Task<IActionResult> GetDetails2(string flag,string para1,string para2)
+        public async Task<IActionResult> GetDetails2([FromRoute] string flag,string para1,string para2)
         {
             var employeeDetailsResult = await _employeeModuleLogic.GetDetailsSer2(flag,para1,para2);
             if ((employeeDetailsResult is IActionResult actionResult))
@@ -41,6 +42,115 @@ namespace ApiExample1.Controllers
             }
             return BadRequest();
         }
+        [HttpPost("PostDetailsemp")]
+        public async Task<IActionResult> PostDetailsemp(string flag, string para1, string para2,string para3,string para4)
+        {
+            var employeeDetailsResult = await _employeeModuleLogic.PostDetailsempser1(flag, para1, para2,para3,para4);
+            if ((employeeDetailsResult is IActionResult actionResult))
+            {
+                return actionResult;
+            }
+            return BadRequest();
+        }
+        //[HttpPost("PostDetails2")]
+        //public async Task<IActionResult> PostDetails2([FromBody]EmployeeReqDto postdto)
+        //{
+        //    var employeeDetailsResult = await _employeeModuleLogic.PostDetailsempserM2(postdto);
+        //    if ((employeeDetailsResult is IActionResult actionResult))
+        //    {
+        //        return actionResult;
+        //    }
+        //    return BadRequest();
+        //}
+        [HttpPost("PostDetails2")]
+        public async Task<IActionResult> PostDetails2([FromBody] EmployeeReqDto postdto)
+        {
+            //if (!ModelState.IsValid)
+            //{
+            //    return BadRequest(ModelState);
+            //}
+            if(!ModelState.IsValid)
+            {
+                var errors = ModelState
+                    .Where(e => e.Value.Errors.Count > 0)
+                    .Select(e => new
+                    {
+                        Field = e.Key,
+                        Messages = e.Value.Errors.Select(err => err.ErrorMessage).ToArray()
+                    });
+                return BadRequest(new
+                {
+                    Message = "validation failed",
+                    Errors = errors
+                });
+            }
+            else
+            { 
+                  var employeeDetailsResult = await _employeeModuleLogic.PostDetailsempserM2(postdto);
+            if ((employeeDetailsResult is IActionResult actionResult))
+            {
+                return actionResult;
+            }
+            return BadRequest();
+        }
+
+                return BadRequest();
+        }
+        
+
+        //[HttpPut("PutDetailsemp")]
+        //public async Task<IActionResult> PutDetailsemp(string flag, string para1, string para2, string para3, string para4)
+        //{
+        //    var employeeDetailsResult = await _employeeModuleLogic.PutDetailsempser11(flag, para1, para2, para3, para4);
+        //    if ((employeeDetailsResult is IActionResult actionResult))
+        //    {
+        //        return actionResult;
+        //    }
+        //    return BadRequest();
+        //}
+        [HttpPost("PostDetailsDocImg")]
+        public async Task<IActionResult> PostDetailsDocImg(ImageReqDto postreq1 )
+        {
+            var employeeDetailsResult = await _employeeModuleLogic.PostDetailsDocser11(postreq1);
+            if ((employeeDetailsResult is IActionResult actionResult))
+            {
+                return actionResult;
+            }
+            return BadRequest();
+        }
+
+        [HttpPut("PutDetailsupdate")]
+        public async Task<IActionResult> PutDetailsupdate(EmployeeReqDto putdto)
+        {
+            var employeeDetailsResult = await _employeeModuleLogic.PutDetailsempserup2(putdto);
+            if ((employeeDetailsResult is IActionResult actionResult))
+            {
+                return actionResult;
+            }
+            return BadRequest();
+        }
+        [HttpDelete("DeleteDetailsEmp")]
+        public async Task<IActionResult> DeleteDetailsEmp(string flag,string para1)
+        {
+            var employeeDetailsResult = await _employeeModuleLogic.DeleteDetailsempserup2(flag,para1);
+            if ((employeeDetailsResult is IActionResult actionResult))
+            {
+                return actionResult;
+            }
+            return BadRequest();
+        }
+        [HttpDelete("DeleteDetailsDocImg")]
+        public async Task<IActionResult> DeleteDetailsDocImg(ImageReqDto deldto)
+        {
+            var employeeDetailsResult = await _employeeModuleLogic.DeleteDetailsDocser11(deldto);
+            if ((employeeDetailsResult is IActionResult actionResult))
+            {
+                return actionResult;
+            }
+            return BadRequest();
+        }
+
+       
 
     }
 }
---
+++ ProjectModuleController.cs
@@ -0,0 +1,121 @@
+﻿using Business.Logic;
+using DataAccess.Models;
+using DataAccess.Repository;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ApiExample1.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProjectModuleController : ControllerBase
+    {
+        private readonly ProjectModuleLogic _projectModuleLogic;
+
+        public ProjectModuleController(ProjectModuleLogic projectModuleLogic)
+        {
+            _projectModuleLogic = projectModuleLogic;
+        }
+        [HttpGet("GetDetailsproject")]
+        public async Task<IActionResult> GetDetailsproject(string flag, string para1, string para2,string para3,string para4)
+        {
+            var projectDetailsResult = await _projectModuleLogic.GetDetailsLog2(flag, para1, para2,para3,para4);
+            if ((projectDetailsResult is IActionResult actionResult))
+            {
+                return actionResult;
+            }
+            return BadRequest();
+        }
+        [HttpGet("GetDeatilsQuery")]
+        public async Task<dynamic> GetDetails111()
+        {
+            var ProjectDetails = await _projectModuleLogic.GetDetailsLogic111();
+            if (ProjectDetails == null)
+            {
+                return NotFound();
+            }
+            return Ok(ProjectDetails);
+
+        }
+        [HttpPost("PostDetailsProject")]
+        public async Task<IActionResult> PostDetailsProject(string flag, string para1, string para2, string para3, string para4, string para5)
+        {
+            var ProjectDetailsResult = await _projectModuleLogic.PostDetailsProjectser1(flag, para1, para2, para3, para4, para5);
+            if (ProjectDetailsResult is IActionResult actionResult)
+            {
+                return actionResult;
+            }
+            return BadRequest();
+        }
+        [HttpPost("PostDetailsM2")]
+        public async Task<IActionResult> PostDetailsM2([FromBody] ProjectReqDto Opostdto)
+        {
+            var ProjectDetailsResult = await _projectModuleLogic.PostDetailsProjectserM2(Opostdto);
+            if ((ProjectDetailsResult is IActionResult actionResult))
+            {
+                return actionResult;
+            }
+            return BadRequest();
+        }
+        [HttpPost("PostDetailsDoc")]
+        public async Task<IActionResult> PostDetailsDoc([FromBody] DocReqdto postreqDoc)
+        {
+            var ProjectDetailsResult = await _projectModuleLogic.PostDetailsProjectserM2(postreqDoc);
+            if ((ProjectDetailsResult is IActionResult actionResult))
+            {
+                return actionResult;
+            }
+            return BadRequest();
+        }
+        [HttpPut("PutDetailsImageproupdate")]
+        public async Task<IActionResult> PutDetailsImageproupdate(ImageReqDto postreqImage)
+        {
+            var ProjectDetailsResult = await _projectModuleLogic.PostDetailsProjectUpdate(postreqImage);
+            if ((ProjectDetailsResult is IActionResult actionResult))
+            {
+                return actionResult;
+            }
+            return BadRequest();
+        }
+        [HttpDelete("DeleteDetailsprojectCon")]
+        public async Task<IActionResult> DeleteDetailsprojectCon(string flag,string para1)
+        {
+            var projectDetailsResult = await _projectModuleLogic.DeleteDetailsproser(flag,para1);
+            if ((projectDetailsResult is IActionResult actionResult))
+            {
+                return actionResult;
+            }
+            return BadRequest();
+        }
+        [HttpDelete("DeleteDetailsDocProj")]
+        public async Task<IActionResult> DeleteDetailsDocProj(DocReqdto deldocdto)
+        {
+            var ProjectDetailsResult = await _projectModuleLogic.DeleteDetailsProDocSer(deldocdto);
+            if ((ProjectDetailsResult is IActionResult actionResult))
+            {
+                return actionResult;
+            }
+            return BadRequest();
+        }
+        [HttpPut("PutDetailsupdateProject")]
+        public async Task<IActionResult> PutDetailsupdateProject(ProjectReqDto putprojdto)
+        {
+            var ProjectDetailsResult = await _projectModuleLogic.PutDetailsProjDto(putprojdto);
+            if ((ProjectDetailsResult is IActionResult actionResult))
+            {
+                return actionResult;
+            }
+            return BadRequest();
+        }
+
+        [HttpPut("updateDetailsProjectDocument")]
+        public async Task<IActionResult> updateDetailsProject(DocReqdto putDocdto)
+        {
+            var ProjectDetailsResult = await _projectModuleLogic.UpdateDocSer(putDocdto);
+            if ((ProjectDetailsResult is IActionResult actionResult))
+            {
+                return actionResult;
+            }
+            return BadRequest();
+        }
+    }
+}
