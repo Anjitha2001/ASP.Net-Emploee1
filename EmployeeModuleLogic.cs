b95215ef85b1b65453f9044e97e2a528e5fd76fd
ANJITHA RAJ <anjitharaj29@gmail.com>
2025-07-01 08:43:10 +0530

    Add files via upload

--- EmployeeModuleLogic.cs
+++ EmployeeModuleLogic.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Reflection.Metadata.Ecma335;
 using System.Text;
 using System.Threading.Tasks;
+using DataAccess.Models;
 using DataAccess.Repository;
 using Microsoft.AspNetCore.Mvc;
 
@@ -44,5 +45,105 @@ namespace Business.Logic
             return Ok(EmployeeDetails);
             
         }
+        public async Task<IActionResult> PostDetailsempser1(string flag, string para1, string para2, string para3,
+            string para4)
+        {
+            var EmployeeDetails= await _employeeModuleRepo.PostDetailsEmpRepo1(flag, para1, para2,para3,para4);
+            if (EmployeeDetails == null)
+            {
+
+
+                return NotFound();
+            }
+            return Ok(EmployeeDetails);
+
+        }
+        public async Task<IActionResult> PostDetailsempserM2(EmployeeReqDto postdto)
+           
+        {
+            var EmployeeDetails = await _employeeModuleRepo.PostDetailsEmpRepoM2(postdto);
+            if (EmployeeDetails == null)
+            {
+
+
+                return NotFound();
+            }
+            return Ok(EmployeeDetails);
+
+        }
+        //public async Task<IActionResult> PutDetailsempser11(string flag, string para1, string para2, string para3,
+        //    string para4)
+        //{
+        //    var EmployeeDetails = await _employeeModuleRepo.PutDetailsEmpRepo11(flag, para1, para2, para3, para4);
+        //    if (EmployeeDetails == null)
+        //    {
+
+
+        //        return NotFound();
+        //    }
+        //    return Ok(EmployeeDetails);
+
+        //}
+        public async Task<IActionResult> PostDetailsDocser11(ImageReqDto postreq1)
+        {
+            var EmployeeDetails = await _employeeModuleRepo.PostImageRepo(postreq1);
+            if (EmployeeDetails == null)
+            {
+
+
+                return NotFound();
+            }
+            return Ok(EmployeeDetails);
+
+        }
+        public async Task<IActionResult> PutDetailsempserup(EmployeeReqDto putdto)
+        {
+            var EmployeeDetails = await _employeeModuleRepo.PutDetailsEmpRepoup(putdto);
+            if (EmployeeDetails == null)
+            {
+
+
+                return NotFound();
+            }
+            return Ok(EmployeeDetails);
+
+        }
+        public async Task<IActionResult> PutDetailsempserup2(EmployeeReqDto putdto)
+        {
+            var EmployeeDetails = await _employeeModuleRepo.PutDetailsEmpRepoup(putdto);
+            if (EmployeeDetails == null)
+            {
+
+
+                return NotFound();
+            }
+            return Ok(EmployeeDetails);
+
+        }
+        public async Task<IActionResult> DeleteDetailsempserup2(string flag, string para1)
+        {
+            var EmployeeDetails = await _employeeModuleRepo.DeleteDetailsEmpRepo11(flag,para1);
+            if (EmployeeDetails == null)
+            {
+
+
+                return NotFound();
+            }
+            return Ok(EmployeeDetails);
+
+        }
+        public async Task<IActionResult> DeleteDetailsDocser11(ImageReqDto deldto)
+        {
+            var EmployeeDetails = await _employeeModuleRepo.DeleteDetailsEmpRepoDto(deldto);
+            if (EmployeeDetails == null)
+            {
+
+
+                return NotFound();
+            }
+            return Ok(EmployeeDetails);
+
+        }
+
     }
 }
---
+++ ProjectModuleLogic.cs
@@ -0,0 +1,144 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DataAccess.Models;
+using DataAccess.Repository;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Business.Logic
+{
+    public class ProjectModuleLogic:ControllerBase
+    {
+        private readonly projectModuleRepo _projectModuleRepo;
+
+        public ProjectModuleLogic(projectModuleRepo projectModuleRepo)
+        {
+            _projectModuleRepo = projectModuleRepo;
+
+
+        }
+        public async Task<dynamic> GetDetailsLogic111()
+        {
+            var ProjectDetails = await _projectModuleRepo.GetDetailsRepo434();
+            if (ProjectDetails == null)
+            {
+                return NotFound();
+            }
+            return Ok(ProjectDetails);
+
+        }
+        public async Task<IActionResult> GetDetailsLog2(string flag, string para1, string para2,string para3,string para4)
+        {
+            var ProjectDetails = await _projectModuleRepo.GetDetailsPro(flag, para1, para2,para3,para4);
+            if (ProjectDetails == null || !ProjectDetails.Any())
+            {
+
+
+                return NotFound();
+            }
+            return Ok(ProjectDetails);
+
+        }
+        public async Task<IActionResult> PostDetailsProjectser1(string flag, string para1, string para2, string para3,
+           string para4, string para5)
+        {
+            var ProjectDetails = await _projectModuleRepo.PostDetailsProjectRepo1(flag, para1, para2, para3, para4,para5);
+            if (ProjectDetails == null)
+            {
+
+
+                return NotFound();
+            }
+            return Ok(ProjectDetails);
+
+        }
+        public async Task<IActionResult> PostDetailsProjectserM2(DocReqdto postreqDoc)
+
+        {
+            var ProjectDetails = await _projectModuleRepo.PostDocrepo(postreqDoc);
+            if (ProjectDetails == null)
+            {
+
+
+                return NotFound();
+            }
+            return Ok(ProjectDetails);
+
+        }
+        public async Task<IActionResult> PostDetailsProjectserM2(ProjectReqDto Opostdto)
+
+        {
+            var ProjectDetails = await _projectModuleRepo.PostDetailsProjectRepoM2(Opostdto);
+            if (ProjectDetails == null)
+            {
+
+
+                return NotFound();
+            }
+            return Ok(ProjectDetails);
+
+        }
+        public async Task<IActionResult> PostDetailsProjectUpdate(ImageReqDto postreqImage)
+
+        {
+            var ProjectDetails = await _projectModuleRepo.PostImageRepo(postreqImage);
+            if (ProjectDetails == null)
+            {
+
+
+                return NotFound();
+            }
+            return Ok(ProjectDetails);
+        }
+        public async Task<IActionResult> DeleteDetailsproser(string flag,string para1)
+        {
+            var ProjectDetails = await _projectModuleRepo.DeleteDetailsProject(flag,para1);
+            if (ProjectDetails == null)
+            {
+
+
+                return NotFound();
+            }
+            return Ok(ProjectDetails);
+
+        }
+        public async Task<IActionResult> DeleteDetailsProDocSer(DocReqdto deldocdto)
+        {
+            var ProjectDetails = await _projectModuleRepo.DeleteDetailsProDoc(deldocdto);
+            if (ProjectDetails == null)
+            {
+
+
+                return NotFound();
+            }
+            return Ok(ProjectDetails);
+
+        }
+        public async Task<IActionResult> PutDetailsProjDto(ProjectReqDto putprojdto)
+        {
+            var ProjectDetails = await _projectModuleRepo.PutDetailsProjDto(putprojdto);
+            if (ProjectDetails == null)
+            {
+
+
+                return NotFound();
+            }
+            return Ok(ProjectDetails);
+
+        }
+        public async Task<IActionResult> UpdateDocSer(DocReqdto putDocdto)
+        {
+            var ProjectDetails = await _projectModuleRepo.UpdateDetailsProDoc(putDocdto);
+            if (ProjectDetails == null)
+            {
+
+
+                return NotFound();
+            }
+            return Ok(ProjectDetails);
+
+        }
+    }
+    }
